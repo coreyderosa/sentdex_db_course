9748a7fa9b8742c5b23314729cb717ba05f30be1
coreyderosa <coreyderosa@gmail.com>
2016-10-08 21:23:13 -0600

    2nd and 3rd video
    Inserted random data into table and created query to retrieve data

--- sqlite-1.py
+++ sqlite-1.py
@@ -1,4 +1,7 @@
 import sqlite3
+import time
+import datetime
+import random
 
 conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
@@ -9,13 +12,38 @@ def create_table():
 def data_entry():
     c.execute("INSERT INTO stuffToPlot VALUES(3213254231, '2016-01-02', 'Python', 8)")
     conn.commit() #need to run a commit whenever any update is made to a db
-    
-    #c.close()
-    #conn.close()
+    c.close()
+    conn.close()
+
+def dynamic_data_entry():
+    unix = time.time()
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    keyword = 'Python'
+    value = random.randrange(0,10)
+    c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?,?,?,?)",
+              (unix, date, keyword, value))
+    conn.commit()
+
+def read_from_db(): #runs a query that shows all data in our table
+    c.execute('SELECT * FROM stuffToPlot WHERE value = 3 OR value = 7')
+    #data = c.fetchall()
+    #print(data)
+    for row in c.fetchall(): #this prettys up the db in Python shell by printing every row separately
+        print(row[0]) #prints only the first column [0] in the db
 
 
 
-create_table()
-data_entry()
 
+
+
+#alt + 3 will comment out selected text
+##create_table()
+###data_entry()
+##for i in range(10):
+##    dynamic_data_entry()
+##    time.sleep(1)
+##    
+read_from_db()
+c.close()
+conn.close()
     
