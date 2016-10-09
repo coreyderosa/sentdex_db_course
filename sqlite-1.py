986efce48b4b52bba84c7438a02430c4b3fba33e
coreyderosa <coreyderosa@gmail.com>
2016-10-09 12:39:56 -0600

    Deleted values
    Deleted all values of 99.  Also, printed ######### to show separation of
    old and updated db

--- sqlite-1.py
+++ sqlite-1.py
@@ -59,7 +59,12 @@ def del_and_update():
 ##    c.execute('SELECT * FROM stuffToPlot')
 ##    [print(row) for row in c.fetchall()]
 
-    
+    c.execute('DELETE FROM stuffToPlot WHERE value = 99')
+    conn.commit()
+    print(50* '#')
+
+    c.execute('SELECT * FROM stuffToPlot')
+    [print(row) for row in c.fetchall()]          
 
 #alt + 3 will comment out selected text
 ##create_table()
